0d2dc150177a128b72676ede3882362f2d53a8c2
ABASSE <barry96mamoudou@gmail.com>
2025-11-07 11:24:54 +0100

    activation du cors au run.py a nouveau

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,9 @@ import os
 from flask_cors import CORS
 
 app = create_app()
-CORS(app)
+
+# ✅ Autoriser toutes les origines et toutes les routes
+CORS(app, resources={r"/*": {"origins": "*"}})
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)

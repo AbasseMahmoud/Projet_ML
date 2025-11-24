61b8ef9132db4c4224cb59cb32f7f23e802ffb01
ABASSE <barry96mamoudou@gmail.com>
2025-11-24 18:10:07 +0100

    modification element du back

--- backend/app/wsgi.py
+++ backend/app/wsgi.py
@@ -1,5 +1,10 @@
-# wsgi.py
-from app import app  # Remplace 'app' par le nom de ton fichier principal Flask si différent
+# # wsgi.py
+# from app import app  # Remplace 'app' par le nom de ton fichier principal Flask si différent
+
+# if __name__ == "__main__":
+#     app.run()
+
+from app import app  # Importe l'app depuis app.py
 
 if __name__ == "__main__":
     app.run()
